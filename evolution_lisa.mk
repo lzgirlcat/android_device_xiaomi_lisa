6389e5f3323c3d18a5ddeb2617919515552b5963
Lily <lz@girlc.at>
2022-06-20 19:22:06 +0200

    evox changes

--- evolution_lisa.mk
+++ evolution_lisa.mk
@@ -8,17 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from davinci device
+# Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := 2109119DG
-PRODUCT_NAME := lineage_lisa
+PRODUCT_MODEL := 11 Lite 5G NE
+PRODUCT_NAME := evolution_lisa
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
